506785c292a4627f8d89c96d17ca36ad37ee13cd
Ben <van.citters.b@gmail.com>
2022-08-20 00:31:51 -0500

    jeeze weeks' late initial commit :p

---
+++ quakec/copyDat.bat
@@ -0,0 +1,7 @@
+copy %CD%\progs.dat "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\mpmod"\progs.dat
+copy %CD%\progs.dat "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\Id1"\progs.dat
+
+
+"c:\Program Files (x86)\Steam\steamapps\common\Quake\rerelease\Quake_x64_steam.exe" -game mpmod
+
+timeout /T 2
